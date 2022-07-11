bcb97ff30ecbf44631bbd4a7197db0fec3b88848
Oliver <oliver.eisenberg88@gmail.com>
2022-07-11 17:58:40 +0100

    Refactoring Aged Brie past sell date

--- Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
@@ -41,20 +41,22 @@ class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                switch (items[i].name) {
+                    case "Aged Brie":
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                        break;
+                    default:
+                        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (items[i].quality > 0) {
+                                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                                    items[i].quality = items[i].quality - 1;
+                                }
+                            }
+                        } else {
+                            items[i].quality = items[i].quality - items[i].quality;
+                        }
                 }
             }
         }
