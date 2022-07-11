fc7b18d491ff597e5b61217e1fa94c032bd644c2
Oliver <oliver.eisenberg88@gmail.com>
2022-07-11 18:47:18 +0100

    Changed to a Switch statement

--- Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
@@ -9,11 +9,16 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals("Aged Brie") || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            switch (items[i].name) {
+                case "Aged Brie":
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
                         if (items[i].sellIn < 11) {
                             items[i].quality = items[i].quality + 1;
                         }
@@ -22,14 +27,15 @@ class GildedRose {
                             items[i].quality = items[i].quality + 1;
                         }
                     }
-                }
-            } else {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                default:
+                    if (items[i].quality > 0) {
                         items[i].quality = items[i].quality - 1;
                     }
-                }
             }
+
 
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
