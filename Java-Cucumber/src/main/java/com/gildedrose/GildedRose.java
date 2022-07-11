91855f576416709349a43ed2e16c3d251957085d
Oliver <oliver.eisenberg88@gmail.com>
2022-07-11 18:57:41 +0100

    Updated item loop for clarity when refs each item

--- Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
@@ -8,23 +8,23 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
+        for (Item item : this.items){
 
-            switch (items[i].name) {
+            switch (item.name) {
                 case "Aged Brie":
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                     break;
                 case "Backstage passes to a TAFKAL80ETC concert":
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                        if (items[i].sellIn < 11) {
-                            items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                        if (item.sellIn < 11) {
+                            item.quality = item.quality + 1;
                         }
 
-                        if (items[i].sellIn < 6) {
-                            items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 6) {
+                            item.quality = item.quality + 1;
                         }
                     }
                     break;
@@ -32,32 +32,32 @@ class GildedRose {
                     // Quality never changes.
                     break;
                 default:
-                    if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
                     }
             }
 
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                switch (items[i].name) {
+            if (item.sellIn < 0) {
+                switch (item.name) {
                     case "Aged Brie":
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
                         break;
                     case "Backstage passes to a TAFKAL80ETC concert":
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                         break;
                     case "Sulfuras, Hand of Ragnaros":
                         // Quality never changes.
                         break;
                     default:
-                        if (items[i].quality > 0) {
-                            items[i].quality = items[i].quality - 1;
+                        if (item.quality > 0) {
+                            item.quality = item.quality - 1;
                         }
                 }
             }
